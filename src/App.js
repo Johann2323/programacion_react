9d708898d4d9705f41339fa59af2ce41d4f8649d
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2023-01-14 12:03:19 -0500

    Interfaz

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect, userCallback} from 'react';
-import { Menubar } from 'primereact/menubar';
-import { InputText } from 'primereact/inputtext';
+import React, { useState, useEffect, userCallback } from 'react';
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-const UserProfiles=()=>{
-  const fetchUserPfroile=()=>{
-    axios.get('http://localhost:8080/api/cursos').then(res=>{
+const UserProfiles = () => {
+  const fetchUserPfroile = () => {
+    axios.get('http://localhost:8080/api/cursos').then(res => {
       console.log(res)
     });
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUserPfroile();
   }, []);
   return <h1>Hello</h1>;
@@ -63,62 +63,79 @@ root.render(<FileInput />);
 */
 
 function App() {
- 
-  const [nombre,setNombre]=useState("Codigo");
-  const cambiarNombre=(e)=>{
-    const value= e.target.value
+
+  const [nombre, setNombre] = useState("Codigo");
+  const cambiarNombre = (e) => {
+    const value = e.target.value
     console.log(value)
     setNombre(value);
   }
 
-  
 
-  const guardarClick=()=>{
-    console.log("Esta es mi estado:",nombre);
+
+  const guardarClick = () => {
+    console.log("Esta es mi estado:", nombre);
     axios.post('http://localhost:8080/api/assets/upload?file')
   };
 
-  const [archivos, setArchivos]=useState(null)
-  const subirArchivos =e=>{
+  const [archivos, setArchivos] = useState(null)
+  const subirArchivos = e => {
     setArchivos(e);
     console.log(e);
-    
+
   }
-  const insertarArchivos=async()=>{
+  const insertarArchivos = async () => {
 
     const f = new FormData();
 
     for (let index = 0; index < archivos.length; index++) {
-      f.append("file",archivos[index]);
-      
+      f.append("file", archivos[index]);
+
     }
 
-    console.log("Aqui "+f)
-    await axios.post("http://localhost:8080/api/assets/upload", f,{headers:{'Content-Type':'multipart/form-data'}})
-    
-    .then(response =>{
-      console.log(response.data);
-      console.log(f)
-    }).catch(error=>{
-      console.log(error);
-    })
+    console.log("Aqui " + f)
+    await axios.post("http://localhost:8080/api/assets/upload", f, { headers: { 'Content-Type': 'multipart/form-data' } })
+
+      .then(response => {
+        console.log(response.data);
+        console.log(f)
+      }).catch(error => {
+        console.log(error);
+      })
   }
   return (
 
-    <div className="App">
+    <div className="App" style={{
+      backgroundImage: `url("https://www.wallpapertip.com/wmimgs/81-810873_green-color-lightness-and-darkness-differs-single-color.jpg")`
+    }}>
       <header>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <h1>Cargar un libro</h1>
-      <label>Nombre del libro:</label>
-      
-      <InputText id="nombre" name="nombre" value={nombre} onChange={cambiarNombre}></InputText>
-      <button onClick={guardarClick}>Guardar</button>
-      <input type="file" id="subir" name="PDF" onChange={(e)=>subirArchivos(e.target.files)}/>
-      <button onClick={()=>insertarArchivos()}>Guardar Libro</button>
+      <h1 >Libros</h1>
+      <br></br><br></br>
+      <label>Titulo del Libro:</label>&nbsp;&nbsp;&nbsp;
+      <b-field expanded>
+        <input class="input"
+          placeholder="Nombre" type="text" />
+      </b-field><br></br><br></br>
 
-  
-      
+      <label>Autor:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <b-field expanded>
+        <input class="input"
+          placeholder="Nombre" type="text" />
+      </b-field><br></br><br></br>
+
+      <label>Descripción:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <b-field expanded>
+        <input class="input"
+          placeholder="Nombre" type="text" />
+      </b-field><br></br><br></br>
+
+      <input type="file" id="subir" name="PDF" onChange={(e) => subirArchivos(e.target.files)} /><br></br>
+      <br></br><br></br>
+      <Button variant="success" onClick={() => insertarArchivos()}>Guardar Libro</Button>{' '}
+      <br></br><br></br>
+      <img src="https://cdn-icons-png.flaticon.com/512/3143/3143460.png" width={200} height={200}></img>
 
     </div>
   );
